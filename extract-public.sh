2b7a15a010a29889aa61ac83709910f4eb5038e3
Martindale, Nathan <nathanamartindale@gmail.com>
2025-02-22 22:24:18 -0500

    Grab and copy any referenced attachments from public notes

--- extract-public.sh
+++ extract-public.sh
@@ -7,7 +7,7 @@ mkdir ${script_path}/notes
 
 pushd ~/lazuli
 
-rg "#public" --no-heading --sort=path | cut -d : -f 1 | uniq | xargs -I src cp src ${script_path}/notes/
+rg "#public" --no-heading --sort=path | cut -d : -f 1 | uniq | xargs -I src cp src "${script_path}/notes/"
 
 # get list of names
 names="$(ls ${script_path}/notes/*.md)"
@@ -20,5 +20,9 @@ git ls-tree -r --name-only HEAD | while read filename; do
     echo "${filename}:${modify_time}" >> "${script_path}/notes/editdata.dat"
   fi
 done
+
+# extract any images/attachments found (anything with an actual extension)
+rg "#public" --no-heading --sort=path | cut -d : -f 1 | uniq | xargs -d "\n" rg --no-heading -e "\[\[.*\.(jpg|png|svg|JPG|PNG|SVG|txt|pdf)\]\]" | cut -d : -f 2 | sed -e "s/.*\[\[\([a-zA-Z0-9\-\_\.]*\)\]\]/\1/g" | xargs -I src cp attachments/src "${script_path}/notes/"
+
 
 popd
