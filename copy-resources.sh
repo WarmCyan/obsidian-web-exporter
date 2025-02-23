4f1c6b6cf63abb6b267e7e5b1c8bf07ae07f530c
Martindale, Nathan <nathanamartindale@gmail.com>
2025-02-22 21:53:36 -0500

    Add robots.txt

--- copy-resources.sh
+++ copy-resources.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# script_path="$(dirname $(realpath $0))"
-
 mkdir -p output/res
 cp -r assets/fonts output/res
 cp assets/style.css output/res
 cp assets/code-syntax-hl.css output/res
+cp assets/robots.txt output
